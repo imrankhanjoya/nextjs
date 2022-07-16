765dbefd014bbcd49e05d0f294ff82026b849c58
Imran Khan <imran.khan.joya@gmail.com>
2022-07-17 01:03:56 +0530

    all

--- components/Loading.js
+++ components/Loading.js
@@ -1,6 +1,6 @@
-const Loading = () => {
+const Loading = ({ status }) => {
     return (
-        <div className="w-full text-center">Processing...</div>
+        <div className="w-full text-center h-full top-0 right-0 left-0 bg-opacity-75 z-100 fixed h-full bg-black flex flex-row" ><img src="/4Mg9.gif" className="w-1/2 opacity-25" /><span className="text-[30px] align-text-middle mt-[10%] h-full">processing....{status}</span></div>
     )
 }
 
--- components/Nft.js
+++ components/Nft.js
@@ -23,26 +23,28 @@ const Nft = ({ item }) => {
 		const contract = new ethers.Contract(nft_market_address, NFT_MARKET, signer)
 		console.log(contract)
 
-		const price = ethers.utils.parseUnits(price.toString(), 'ether')
+		const price_buy = ethers.utils.parseUnits(price.toString(), 'ether')
 
 		await contract.createMarketSale(nftContract, itemId, {
-			value: price,
+			value: price_buy,
 			gasLimit: 100000,
 			nonce: undefined,
 		})
 	}
 	return (
 		<>
+			{console.log(item.tokenId._hex)}
 			<div className="rounded-lg  overflow-hidden">
 				<img alt="content" className="object-cover object-center h-full w-full" src={item.image} />
 			</div>
 			<div className="flex flex-row space-x-5 p-2">
-				<label className="text-green-600">Price: {item.price} eth</label>
-				<button onClick={() => makepurchase(item.nftContract, item.tokenId, item.price)} className="inline-flex items-center bg-green-100 border-0 mt-4  px-3 focus:outline-none hover:bg-green-200 rounded text-sm font-bold md:mt-0">Buy</button>
+				<div className="text-green-600">Price: {item.price} eth</div>
+				<button onClick={() => makepurchase(item.nftContract, item.tokenId._hex, item.price)} className="inline-flex items-center bg-green-100 border-0 mt-4  px-3 focus:outline-none hover:bg-green-200 rounded text-sm font-bold md:mt-0">Buy</button>
 			</div>
 			<div className="flex flex-col">
-				<small className="text-sm break-words	">Seller:{item.seller}</small>
-				<small className="text-sm break-words	">Owner: {item.owner}</small>
+				<div className="text-sm break-words">tokenid:{item.tokenId._hex}</div>
+				<div className="text-sm break-words	">Seller:{item.seller}</div>
+				<div className="text-sm break-words	">Owner: {item.owner}</div>
 			</div>
 			<h2 className="text-xl font-medium title-font text-gray-900 mt-5">{item.name}</h2>
 			<p className="text-base leading-relaxed mt-2">{item.description}</p>
