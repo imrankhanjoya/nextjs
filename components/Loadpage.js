4d7b96e8e08991511bd832494dddde531b7e9a0e
Imran Khan <imran.khan.joya@gmail.com>
2022-07-23 20:19:10 +0530

    all

--- components/Loadpage.js
+++ components/Loadpage.js
@@ -1,6 +1,6 @@
 const Loadpage = ({ status }) => {
     return (
-        <div className="w-full text-center  h-full bg-black flex flex-row" ><img src="/nft.gif" className="w-1/2 opacity-25" /><span className="text-[30px] align-text-middle mt-[10%] h-full">processing....{status}</span></div>
+        <div className="w-full text-center  h-full bg-black flex flex-row" ><img src="/4Mg9.gif" className="w-1/2 opacity-25" /><span className="text-[30px] align-text-middle mt-[10%] h-full">processing....{status}</span></div>
     )
 }
 
